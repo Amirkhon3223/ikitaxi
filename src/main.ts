03f7dca8445db4ee447cd0f0bf4d49a4b8d3f517
Amirkhon3223 <amirich3223@outlook.com>
2023-08-18 10:42:04 +0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import './assets/tailwind.css'; // Import Tailwind CSS
+
 
 createApp(App).use(router).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+        extend: {
+            colors: {
+                secondary: '#000000',
+                primary: '#00A6ED',
+                white: '#ECECEC',
+            },
+        },
+        variants: {},
+        plugins: [],
+    }
+};
